6e913276eba2cc130ed68e1e64c31bcd25febafc
cg <cg2100773258@163.com>
2023-07-20 19:47:53 +0800

    右边内容样式完成

---
+++ src/components/Main/MainRightWeb.vue
@@ -0,0 +1,113 @@
+<template>
+<!--  TODO 左右内容总div并不会随着动态div而撑开高度-->
+  <div class="main-right">
+    <div class="main-right-content" v-for="(content,index) in article" :key="index">
+      <!-- 左边内容       -->
+      <div v-if="index %2 === 0" class="main-right-item">
+        <img :src="content.img" class="main-right-item-img">
+        <div class="main-right-item-div">
+          {{content.text}}
+        </div>
+      </div>
+
+      <!-- 右边内容     -->
+      <div v-else class="main-right-item">
+        <div class="main-right-item-div">
+          {{content.text}}
+        </div>
+        <img :src="content.img" class="main-right-item-img">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainRightWeb",
+  data(){
+    return {
+      article:[
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容2'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容3'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容2'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容3'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容2'
+        },
+        {
+          img: require('@/assets/main-login_750x300.png'),
+          text:'测试内容3'
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+/*右边样式*/
+.main-right{
+  width: 70%;
+  height: auto;
+  background-color: #323335;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+/*右边子内容样式*/
+.main-right-content{
+  width: 100%;
+  height: auto;
+  background-color:darkslategray;
+}
+
+/*子内容样式*/
+.main-right-item{
+  width: 100%;
+  height: auto;
+  background-color: antiquewhite;
+  margin-bottom: 10px;
+  display: flex;
+}
+
+/*文章列表图片样式*/
+.main-right-item-img{
+  width: 30%;
+  height: 100%;
+}
+
+
+/*文章样式*/
+.main-right-item-div{
+  width: 70%;
+  height: 100%;
+  background-color: green;
+}
+
+
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,78 +1,18 @@
 <template>
   <div class="main">
-    <div class="main-left">
-      <!-- TODO 登陆div     -->
-      <div class="main-login main-div">
-        <!-- 背景图       -->
-        <img src="../assets/main-login_750x300.png" style="width: 100%;height: 40%;">
-        <!-- 登陆展示       -->
-        <div class="main-login-item">
-        <!-- 1、签到；2、积分；         -->
-          登陆前和登陆后展示
-        </div>
-      </div>
-
-      <!-- 公告div     -->
-      <div class="main-notice main-div">
-        <span class="title-text"><i class="el-icon-bell"></i>公告</span>
-        <ul class="main-notice-content" v-for="(value,index) in list" :key="index">
-          <li><i class="el-icon-message-solid"></i>{{value}}</li>
-        </ul>
-      </div>
-
-      <!-- 分类标签div   -->
-      <div class="main-classify main-div">
-        <span class="title-text main-classify-line"><i class="el-icon-collection-tag"></i>标签</span>
-        <div class="main-classify-content">
-          <el-tag type="info" color="white" v-for="(tag,index) in tags" :key="index">
-            {{tag}}
-          </el-tag>
-        </div>
-      </div>
-
-      <!-- 热门推荐文章div     -->
-      <div class="main-hot main-div">
-        <span class="title-text"><i class="el-icon-hot-water"></i>热门文章</span>
-        <div class="main-hot-bottom">
-          <div class="main-hot-content" v-for="(text,index) in content" :key="index">
-            <img src="../assets/main-login_750x300.png" class="main-hot-img">
-            <div class="main-hot-content-item">
-              内容阿萨大大大大大大大水水水水水水水水水水水水水水的大大大大大大阿三顶顶顶顶顶顶顶顶顶顶哇大大{{text}}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- 站点统计div   -->
-      <div class="main-statistics main-div">
-        <span class="title-text"><i class="el-icon-data-line"></i>统计</span>
-        <ul class="main-statistics-content">
-          <li>访问量:</li>
-          用户量
-          内容数
-          运行天数
-        </ul>
-      </div>
-    </div>
-
-    <div class="main-right">
-      右边内容
-    </div>
+    <MainLeftWeb></MainLeftWeb>
+    <MainRightWeb></MainRightWeb>
   </div>
 </template>
 
 <script>
+import MainLeftWeb from "@/components/Main/MainLeftWeb.vue";
+import MainRightWeb from "@/components/Main/MainRightWeb.vue";
+
 export default {
   name: "MainView",
-  data(){
-    return {
-      list:['本站点所用技术:Spring、SpringBoot、Swagger、ElasticSearch、Vue2、Mybatis',
-        '本站点免费，转载本博客文章同需要著名出处，不需要请示，但一定要注明出处',
-        '欢饮来到我的博客网站，希望能帮助到大家，也请大家多多推广我的网站，希望有甲方看到'],
-      tags:['软件设计师','CET4','CET6','红帽认证','软件设计师','CET4','CET6','红帽认证'],
-      content:['1','2','3','4','5']
-    }
-  }
+  components: {MainRightWeb, MainLeftWeb},
+
 }
 </script>
 
@@ -84,109 +24,4 @@ export default {
   display: flex;
   font-size: 14px;
 }
-
-/*左边样式*/
-.main-left{
-  height: 100%;
-  width: 30%;
-  background-color: #f4f4f4;
-  margin-right: 15px;
-}
-/*小div通用样式*/
-.main-div{
-  width: 100%;
-  height: auto;
-  border-radius: 15px;
-  margin-bottom: 10px;
-}
-/*div标题通用样式*/
-.title-text{
-  font-size: 16px;
-  margin: 20px 0 10px 10px;
-}
-
-/*登陆样式*/
-.main-login{
-  background-color: aqua;
-
-}
-/*公告样式*/
-.main-notice{
-  display: flex;
-  flex-direction: column;
-  background-color: sienna;
-}
-/*公告文章内容样式*/
-.main-notice-content{
-  list-style-type: none;
-  margin: 0;
-  padding-left: 20px;
-  text-indent: 10px;
-}
-.main-notice-content li{
-  margin: 2px 10px 2px 0;
-  word-wrap: break-word;
-}
-.main-notice-content:last-child{
-  margin-bottom: 10px;
-}
-
-/*分类标签样式*/
-.main-classify{
-  background-color: aquamarine;
-  display: flex;
-  flex-direction: column;
-}
-/*分类标签内容样式*/
-.main-classify-content .el-tag{
-  margin: 0 5px 5px 10px;
-}
-/*分类下划线样式*/
-.main-classify-line{
-  border-bottom: 1px solid #eaedf4;
-  padding-bottom: 5px;
-}
-
-/*热门文章样式*/
-.main-hot{
-  background-color: greenyellow;
-  display: flex;
-  flex-direction: column;
-}
-/*热门文章内容父div样式*/
-.main-hot-bottom{
-  flex-direction: column;
-  align-items: center;
-  display: flex;
-}
-/*热门文章内容*/
-.main-hot-content{
-  height: 20%;
-  width: 90%;
-  background-color: gray;
-  justify-content: center;
-  display: flex;
-  margin: 10px;
-}
-/*热门文章图表样式*/
-.main-hot-img{
-  width: 30%;
-  height: auto;
-}
-
-/*站点统计样式*/
-.main-statistics{
-  display: flex;
-  flex-direction: column;
-  background-color: palevioletred;
-}
-
-
-/*右边样式*/
-.main-right{
-  width: 70%;
-  height: 100%;
-  background-color: #323335;
-}
-
 </style>
